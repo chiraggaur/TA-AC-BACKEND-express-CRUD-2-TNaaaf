ba60723747f80c190fd5fee22f9c834d62b73828
chirag gaur <chiragwalker@gmail.com>
2023-08-26 13:52:12 +0530

    done

--- block-BNaadl/blog/routes/index.js
+++ block-BNaadl/blog/routes/index.js
@@ -4,7 +4,11 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   data = req.body;
-  res.render("index", { title: "Express" });
+  res.render("index", { title: data });
+});
+router.get("/details", function (req, res, next) {
+  data = req.body;
+  res.render("details", { title: data });
 });
 
 module.exports = router;
